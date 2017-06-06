3c4778763cb7f2d5780e545630c0e46205c1c236
jjurman <jjurman@constantcontact.com>
2017-06-06 03:53:10 -0400

    initial work for reducer example

--- examples/reducers/index.js
+++ examples/reducers/index.js
@@ -1,19 +1,31 @@
 const Tram = require('../../index');
+const xtend = require('xtend');
+
 const app = new Tram();
 
-const html = tram.html({});
+const html = Tram.html({});
 
-const clickReducer = (state, action) {
+const counterReducer = (state, action) => {
   if (action.type === 'click') {
-    return { count: state.count+1 }
+    return xtend(state, { clicks: state.clicks+1 })
   }
+  return state;
 }
 
-const counter = (state) => {
-  return html`<div>
-    This has been clicked ${count} times!
-  </div>`
+const clicker = (state) => {
+  const incrementCount = () => {
+    state.dispatch({type: 'click'});
+  }
+  return html`
+    <div onclick=${incrementCount}>
+      Tram-One uses minidux to handle state management.
+      <br><br>
+
+      This page has been clicked ${state.counter.clicks} times!
+    </div>
+  `
 }
 
-app.route('/', counter);
+app.addReducer('counter', counterReducer, {clicks: 0});
+app.addRoute('/', clicker);
 app.start(document.body);
--- index.js
+++ index.js
@@ -2,26 +2,41 @@ const xtend = require('xtend');
 const nanorouter = require('nanorouter');
 const belCreateElement = require('bel').createElement;
 const rbelRegister = require('rbel');
-const createStore = require('minidux').createStore;
+const minidux = require('minidux');
 
 class Tram {
   constructor() {
     this.router = nanorouter({ default: '/404' });
+    this.reducers = {};
     this.state = {};
   }
 
-  addRoute(path, handler) {
+  addRoute(path, page) {
     this.router.on(path, (pathParams) => {
-      const completeState = xtend(this.state, pathParams);
-      return handler(completeState);
+      const completeState = xtend(
+        this.store.getState(),
+        {dispatch: this.store.dispatch},
+        pathParams
+      );
+      return page(completeState);
     });
   }
 
-  addReducer() {
-
+  addReducer(field, reducer, state) {
+    this.reducers[field] = reducer;
+    this.state[field] = state;
   }
 
   start(target, pathName) {
+    const reducers = minidux.combineReducers(this.reducers);
+    console.log(this.state)
+    this.store = minidux.createStore(reducers, this.state);
+
+    this.store.subscribe( (state) => {
+      // USE YO-YO TO RE-RENDER
+      console.log(state);
+    });
+
     const routePath = pathName || window.location.href.replace(window.location.origin, '');
     const pageComponent = this.router(routePath);
     target.appendChild(pageComponent);
@@ -30,9 +45,7 @@ class Tram {
 }
 
 const html = (registry) => {
-  const render = rbelRegister(belCreateElement, registry || {});
-
-  return render;
+  return rbelRegister(belCreateElement, registry || {});
 }
 
 module.exports = Tram;

3618ecb1ac81701f10a5be415106f35dfa82179b
jjurman <jjurman@constantcontact.com>
2017-06-06 20:24:56 -0400

    example with routes, custom-elements, and reducers

---
+++ examples/all-together/chrome.js
@@ -0,0 +1,15 @@
+const Tram = require('../../index');
+
+const html = Tram.html();
+
+module.exports = (attrs, children) => {
+  const style = 'padding: 0em 2em;';
+  return html`
+    <div>
+      <h1>Tram-One 🚋</h1>
+      <div style=${style}>
+        ${children}
+      </div>
+    </div>
+  `;
+}
---
+++ examples/all-together/color-input.js
@@ -0,0 +1,20 @@
+const Tram = require('../../index');
+
+const html = Tram.html();
+
+const inputStyle = 'font-size: inherit';
+module.exports = (attrs, children) => {
+  const triggerEvent = (event) => {
+    if (event.key === 'Enter') {
+      attrs.onEnterColor(event.currentTarget.value);
+    }
+  }
+  return html`
+    <div>
+      Path:
+      <input  onkeyup=${triggerEvent}
+              style=${inputStyle}
+              value=${window.location.pathname} />
+    </div>
+  `;
+}

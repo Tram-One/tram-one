2d8bb7caa9eeacabd7a584113322370b9c8221a6
jjurman <jjurman@constantcontact.com>
2017-06-10 23:18:41 -0400

    minification, point to dist

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,17 +1,27 @@
+const webpack = require('webpack')
+
 module.exports = {
   entry: './tram-one.js',
   module: {
-    rules: [
-      {
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
+    rules: [{
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
         }
       }
-    ]
+    }]
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
   output: {
     filename: './dist/tram-one.js',
     // export to AMD, CommonJS, or window

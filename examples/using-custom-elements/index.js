6c7bf57708f3351ba5ee92ca92e1e2bc4c7f21a9
jjurman <jjurman@constantcontact.com>
2017-10-18 21:10:49 -0400

    update example code to render correctly

--- examples/using-custom-elements/index.js
+++ examples/using-custom-elements/index.js
@@ -12,17 +12,14 @@ const home = () => {
       This is the custom-elements example!
       <br />
 
-      Tram-One uses
-      <a href="https://github.com/aaaristo/rbel">rbel</a>,
-      <a href="https://github.com/jrjurman/bel-create-element">bel-create-element</a>, and
-      <a href="https://github.com/substack/hyperx">hyperx</a>,
-      to render tagged and custom elements.
-      <br />
+      Tram-One
+      uses <a href="https://github.com/aaaristo/rbel">rbel</a>, <a href="https://github.com/jrjurman/bel-create-element">bel-create-element</a>,
+      and <a href="https://github.com/substack/hyperx">hyperx</a>, to render tagged and custom elements.
+      <br /><br />
 
-      Honestly though, special thanks goes out to
-      <a href="https://github.com/aaaristo">Andrea Gariboldi</a>
+      Honestly though, special thanks goes out
+      to <a href="https://github.com/aaaristo">Andrea Gariboldi</a>
       for building rbel, which does the custom element magic.
-      <br />
     </chrome>
   `
 }

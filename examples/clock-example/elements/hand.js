4858f11b1928fe210637313ec307ff27889cf268
Jesse Jurman <j.r.jurman@gmail.com>
2018-02-05 11:05:58 -0800

    complete clock example

--- examples/clock-example/elements/hand.js
+++ examples/clock-example/elements/hand.js
@@ -12,11 +12,25 @@ const degreesFromAttrs = (attrs) => {
   return 360 * (attrs['value'] / attrs['max-value'])
 }
 
+const halfHand = svg`
+  <defs>
+    <linearGradient id="halfhand" x1=0 x2=0 y1=0 y2=1>
+      <stop offset="0%" />
+      <stop offset="50%" />
+      <stop offset="50%" stop-opacity="0" />
+      <stop offset="100%" stop-opacity="0" />
+    </linearGradient>
+  </defs>
+`
+
 module.exports = (attrs) => svg`
-  <rect
-    x=${50 - (attrs.width/2)} y=${50 - (attrs.radius)}
-    width=${attrs.width} height=${attrs.radius*2}
-    fill=${attrs.color}
-    style=${handStyle(degreesFromAttrs(attrs))}
-  />
+  <g>
+    ${halfHand}
+    <rect
+      fill="url(#halfhand)"
+      x=${50 - (attrs.width/2)} y=${50 - (attrs.radius)}
+      width=${attrs.width} height=${attrs.radius*2}
+      style=${handStyle(degreesFromAttrs(attrs))}
+    />
+  </g>
 `

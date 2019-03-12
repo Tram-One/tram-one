b3343dfcc23434d202c864597d0cc141263b4cd5
JRJurman <jrjurman@gmail.com>
2019-03-11 23:47:59 -0400

    remove most non-static methods

--- tram-one.js
+++ tram-one.js
@@ -15,149 +15,55 @@ const urlListener = require('url-listener')
  */
 class Tram {
   /**
-   * create an instance of a Tram-One app
-   * - for complete documentation, please refer to http://tram-one.io/#constructor
-   *
-   * @param {{defaultRoute: string, webStorage: object}} [options]
-   * @param {string} options.defaultRoute
-   * @param {object} options.webStorage
-   */
-  constructor(options) {
-    if (options) {
-      assert.equal(typeof options, 'object', 'Tram-One: options should be an object')
-    }
-
-    // setup configurations for Tram-One app from options
-    options = options || {}
-    this.defaultRoute = options.defaultRoute || '/404'
-    this.webStorage = options.webStorage
-    this.globalSpace = options.globalSpace || window
-
-    // setup dedicated engine for component state
-    Tram.setupEngine(this.globalSpace, 'stateEngine')
-
-    // setup dedicated engine for app state management
-    Tram.setupEngine(this.globalSpace, 'appEngine')
-
-    // setup store for effects
-    Tram.setupLog(this.globalSpace, 'effectStore')
-
-    // setup router
-    // rlite is the route resolver
-    // the internalRouter lets us build a router configuration over multiple calls
-    this.router = rlite()
-    this.internalRouter = {}
-
-    // setup state-management with HoverEngine
-    // this.engine = new HoverEngine()
-  }
-
-  /**
-   * add actions and store values
-   * - for complete documentation, please refer to http://tram-one.io/#app-add-actions
-   *
-   * @param {object} actionGroups
-   */
-  addActions(actionGroups) {
-    assert.equal(
-      typeof actionGroups, 'object',
-      'Tram-One: ActionGroups should be { store-key: { action-name: action-function } }'
-    )
-
-    this.engine.addActions(actionGroups)
-
-    return this
-  }
-
-  /**
-   * add callback to call whenever an action is triggered
-   * - for complete documentation, please refer tohttp://tram-one.io/#app-add-listener
-   *
-   * @param {(store, actions, actionName, actionArguement) => {}} listener
-   */
-  addListener(listener) {
-    assert.equal(typeof listener, 'function', 'Tram-One: Listener should be a function')
-
-    this.engine.addListener(listener)
-
-    return this
-  }
-
-  /**
-   * add route(s) and subroutes
-   * - for complete documentation, please refer to http://tram-one.io/#app-add-route
-   *
-   * @param {string} path
-   * @param {function} page
-   * @param {Array} [subroutes]
-   */
-  addRoute(path, page, subroutes) {
-    assert.equal(typeof path, 'string', 'Tram-One: path should be a string')
-    assert.equal(typeof page, 'function', 'Tram-One: page should be a function')
-
-    // if subroutes are defined, iterate through them and process them as unique routes
-    if (subroutes) {
-      subroutes.forEach(subroute => {
-        assert.equal(typeof subroute, 'object', 'Tram-One: subroute should be an object, use Tram.route to make subroutes')
-        // if there is an extra '/' when joining subroutes, just fix them
-        const newPath = (path + subroute.path).split('//').join('/')
-        // when building a subrouted-page, first build the subroute
-        // and then pass that into the parent page (the parent can choose how to render the subroute)
-        const newPage = (store, actions, params, resolvedSubroute) => {
-          const newSubroute = subroute.component(store, actions, params, resolvedSubroute)
-          return page(store, actions, params, newSubroute)
-        }
-        this.addRoute(newPath, newPage, subroute.subroutes)
-      })
-    }
-
-    // don't overwrite a route if one already exists
-    // this is important because empty subroutes will match their parent
-    if (this.internalRouter[path]) {
-      console.warn(`Tram-One: path ${path} already exists (this is fine if using empty subroutes)`)
-      return this
-    }
-    // update our internal router, and if this happens to be the last call update the rlite router
-    this.internalRouter[path] = (params) => (store, actions) => page(store, actions, params)
-    this.router = rlite(this.internalRouter[this.defaultRoute], this.internalRouter)
-
-    return this
-  }
-
-  /**
-   * start the app by mounting a path on some DOM or css selector
+   * start the app by mounting a component on some DOM or css selector
    * - for complete documentation, please refer to http://tram-one.io/#app-start
    *
    * @param {*} selector
-   * @param {string} [pathName]
+   * @param {object} component
    */
-  start(selector, pathName) {
-    // re-mount the app when a state action is triggered
-    Tram.getEngine(this.globalSpace, 'stateEngine').addListener(() => {
-      this.mount(selector, pathName)
-    })
+  static start(globalSpace = window) {
 
-    // re-mount the app when an app action is triggered
-    Tram.getEngine(this.globalSpace, 'appEngine').addListener(() => {
-      this.mount(selector, pathName)
-    })
+    // setup dedicated engine for component state
+    Tram.setupEngine(globalSpace, 'stateEngine')
 
-    // if webStorage is defined, wire it into hover-engine with hover-battery
-    // (effectively, set up the initial store values and add appropriate listeners to write to webStorage)
-    if (this.webStorage) {
-      this.engine.addActions(battery(this.webStorage).actions)
-      this.engine.addListener(battery(this.webStorage).listener)
+    // setup dedicated engine for app state management
+    Tram.setupEngine(globalSpace, 'appEngine')
+
+    // setup store for effects
+    Tram.setupLog(globalSpace, 'effectStore')
+
+    // setup working key to match state and effects to components
+    Tram.setupWorkingKey(globalSpace, 'componentKey')
+
+    return (selector, component, webStorage) => {
+      const mount = Tram.mount(globalSpace)
+      // re-mount the app when a state action is triggered
+      Tram.getEngine(globalSpace, 'stateEngine').addListener(() => {
+        mount(selector, component)
+      })
+
+      // re-mount the app when an app action is triggered
+      Tram.getEngine(globalSpace, 'appEngine').addListener(() => {
+        mount(selector, component)
+      })
+
+      // if webStorage is defined, wire it into hover-engine with hover-battery
+      // (effectively, set up the initial store values and add appropriate listeners to write to webStorage)
+      if (webStorage) {
+        Tram.getEngine(globalSpace, 'appEngine').addActions(battery(webStorage).actions)
+        Tram.getEngine(globalSpace, 'appEngine').addListener(battery(webStorage).listener)
+      }
+
+      // wire up urlListener so that we remount whenever the url changes
+      urlListener(() => {
+        mount(selector, component)
+      })
+
+      // trigger an initial mount
+      mount(selector, component)
+
+      return Tram
     }
-
-    // wire up urlListener so that we remount whenever the url changes
-    urlListener(() => {
-      this.mount(selector, pathName)
-    })
-
-    // trigger an initial mount
-    this.mount(selector, pathName)
-
-    return this
   }
 
   /**
@@ -170,123 +76,90 @@ class Tram {
    * @param {object} store
    * @param {object} actions
    */
-  mount(selector, pathName, store, actions) {
-    assert.ok(selector !== undefined, 'Tram-One: selector should be a DOM element or CSS selection string')
+  static mount(globalSpace = window) {
+    return (selector, component) => {
+      assert.ok(selector !== undefined, 'Tram-One: selector should be a DOM element or CSS selection string')
 
-    // if the selector is a string, try to find the element,
-    // otherwise it's probably DOM that we should write directly to
-    const target = (typeof selector) === 'string' ? document.querySelector(selector) : selector
-    if (target === null) {
-      console.warn('Tram-One: could not find target, is the element on the page yet?')
+      // if the selector is a string, try to find the element,
+      // otherwise it's probably DOM that we should write directly to
+      const target = (typeof selector) === 'string' ? document.querySelector(selector) : selector
+      if (target === null) {
+        console.warn('Tram-One: could not find target, is the element on the page yet?')
+      }
+
+      // build a div to render the app on
+      // (if it doesn't exist as a child of the selector, create one first)
+      if (!target.firstElementChild) {
+        const targetChild = document.createElement('div')
+        target.appendChild(targetChild)
+      }
+      const targetChild = target.firstElementChild
+
+      // collect all the DOM events that we should be keeping track of.
+      // these events are provided by belit and consumed by tatermorph.
+      // (events are strange and actually aren't natively stored on Nodes,
+      // but we have to keep track of them so we can know to add or
+      // remove them between renders)
+      const getEvents = (newNode, oldNode) => {
+        return [].concat(newNode.events).concat(oldNode.events)
+      }
+
+      // save all the mount effects that have happened, and wipe the effectStore
+      const existingEffects = Object.assign({}, Tram.getLog(globalSpace, 'effectStore'))
+      Tram.clearLog(globalSpace, 'effectStore')
+
+      // update our child element with a new version of the app
+      // tatermorph knows how to intelligently trigger only diffs that matter on the page
+      morph(targetChild, component(), getEvents)
+
+      // get the effects that are new
+      const allNewEffects = Object.assign({}, Tram.getLog(globalSpace, 'effectStore'))
+
+      // split out effects between existing, new and removed
+      const existingEffectKeys = Object.keys(allNewEffects).filter(effect => (effect in existingEffects))
+      const newEffectKeys = Object.keys(allNewEffects).filter(effect => !(effect in existingEffects))
+      const removedEffectKeys = Object.keys(existingEffects).filter(effect => !(effect in allNewEffects))
+
+      // run all clean up effects if the effect was removed
+      removedEffectKeys.forEach(effectKey => existingEffects[effectKey]())
+
+      // add any effects that should be in the store back in
+      existingEffectKeys.forEach(effectKey => {
+        Tram.getLog(globalSpace, 'effectStore')[effectKey] = existingEffects[effectKey]
+      })
+
+      // run all new effects that we haven't seen before
+      // save any cleanup effects in the effectStore
+      newEffectKeys.forEach(effectKey =>
+        Tram.getLog(globalSpace, 'effectStore')[effectKey] = allNewEffects[effectKey]()
+      )
+
+      return Tram
     }
-
-    // build a div to render the app on
-    // (if it doesn't exist as a child of the selector, create one first)
-    if (!target.firstElementChild) {
-      const targetChild = document.createElement('div')
-      target.appendChild(targetChild)
-    }
-    const targetChild = target.firstElementChild
-
-    // use the pathname provided, or pull the url from the window object
-    const routePath = pathName || window.location.href.replace(window.location.origin, '')
-
-    // collect all the DOM events that we should be keeping track of.
-    // these events are provided by belit and consumed by tatermorph.
-    // (events are strange and actually aren't natively stored on Nodes,
-    // but we have to keep track of them so we can know to add or
-    // remove them between renders)
-    const getEvents = (newNode, oldNode) => {
-      return [].concat(newNode.events).concat(oldNode.events)
-    }
-
-    // save all the mount effects that have happened, and wipe the effectStore
-    const existingEffects = Object.assign({}, Tram.getLog(this.globalSpace, 'effectStore'))
-    Tram.clearLog(this.globalSpace, 'effectStore')
-
-    // update our child element with a new version of the app
-    // tatermorph knows how to intelligently trigger only diffs that matter
-    morph(targetChild, this.toNode(routePath, store, actions), getEvents)
-
-    // get the effects that are new
-    const allNewEffects = Object.assign({}, Tram.getLog(this.globalSpace, 'effectStore'))
-
-    // split out effects between existing, new and removed
-    const existingEffectKeys = Object.keys(allNewEffects).filter(effect => (effect in existingEffects))
-    const newEffectKeys = Object.keys(allNewEffects).filter(effect => !(effect in existingEffects))
-    const removedEffectKeys = Object.keys(existingEffects).filter(effect => !(effect in allNewEffects))
-
-    // run all clean up effects if the effect was removed
-    removedEffectKeys.forEach(effectKey => existingEffects[effectKey]())
-
-    // add any effects that should be in the store back in
-    existingEffectKeys.forEach(effectKey => {
-      Tram.getLog(this.globalSpace, 'effectStore')[effectKey] = existingEffects[effectKey]
-    })
-
-    // run all new effects that we haven't seen before
-    // save any cleanup effects in the effectStore
-    newEffectKeys.forEach(effectKey =>
-      Tram.getLog(this.globalSpace, 'effectStore')[effectKey] = allNewEffects[effectKey]()
-    )
-
-    return this
-  }
-
-  /**
-   * generate a DOM Node Tree based on a path and a store
-   * - for complete documentation, please refer to http://tram-one.io/#app-toNode
-   *
-   * @param {string} pathName
-   * @param {object} [store]
-   * @param {object} [actions]
-   *
-   * @return {HTMLDivElement}
-   */
-  toNode(pathName, store, actions) {
-    assert.equal(typeof pathName, 'string', 'Tram-One: pathName should be a string')
-
-    const pageComponent = this.router(pathName)
-    const pageState = store || Tram.getEngine(this.globalSpace, 'appEngine').store
-    const pageActions = actions || Tram.getEngine(this.globalSpace, 'appEngine').actions
-    return pageComponent(pageState, pageActions)
-  }
-
-  /**
-   * generate a stringified DOM Tree based on a path and a store
-   * - for complete documentation, please refer to http://tram-one.io/#app-toString
-   *
-   * @param {string} pathName
-   * @param {object} [store]
-   *
-   * @return {string}
-   */
-  toString(pathName, store) {
-    return this.toNode(pathName, store).outerHTML
   }
 
   static setupEngine(globalSpace = window, engineName) {
     // we do not have a space to put our engine
     if (!globalSpace) return false
 
-    // we already have an engine, return that one
-    if (globalSpace[engineName]) return globalSpace[engineName]
-
-    // we do not have a engine, make a new one
     globalSpace[engineName] = new HoverEngine()
     return globalSpace[engineName]
   }
 
   static setupLog(globalSpace = window, logName) {
-    // we do not have a space to put our engine
+    // we do not have a space to put our log
     if (!globalSpace) return false
 
-    // we already have a log, return that one
-    if (globalSpace[logName]) return globalSpace[logName]
-
-    // we do not have a log, make a new one
     globalSpace[logName] = {}
     return globalSpace[logName]
+  }
+
+  static setupWorkingKey(globalSpace = window, keyName) {
+    // we do not have a space to put our working key
+    if (!globalSpace) return false
+
+    globalSpace[keyName] = []
+    return globalSpace[keyName]
   }
 
   static getEngine(globalSpace = window, engineName) {
@@ -295,6 +168,10 @@ class Tram {
 
   static getLog(globalSpace = window, logName) {
     return Tram.getEngine(globalSpace, logName)
+  }
+
+  static getWorkingKey(globalSpace = window, keyName) {
+    return Tram.getEngine(globalSpace, keyName)
   }
 
   static clearLog(globalSpace = window, logName) {
@@ -371,6 +248,16 @@ class Tram {
     }
   }
 
+  static addListener(globalSpace = window, engineName = 'appEngine') {
+    return (listener) => {
+      assert.equal(typeof listener, 'function', 'Tram-One: Listener should be a function')
+
+      Tram.getEngine(globalSpace, engineName).addListener(listener)
+
+      return Tram
+    }
+  }
+
   static route(getPath = () => window.location.href.replace(window.location.origin, '')) {
     return (attrs, children) => {
       return rlite(() => '', {
@@ -424,16 +311,6 @@ class Tram {
   static svg(registry) {
     return Tram.dom('http://www.w3.org/2000/svg', registry)
   }
-
-  /**
-   * function to create a subroute generator
-   * - for complete documentation, please refer to http://tram-one.io/#tram-route
-   *
-   * @return {(path: string, component: function, subroutes?: Array) => {path, component, subroutes}}
-   */
-  // static route() {
-  //   return (path, component, subroutes) => ({path, component, subroutes})
-  // }
 }
 
 module.exports = Tram

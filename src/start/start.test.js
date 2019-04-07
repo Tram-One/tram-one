533a41ae38bdca8f8f82f8d475e4e4591fb27fcf
JRJurman <jrjurman@gmail.com>
2019-04-07 03:23:19 -0400

    initial tests for start

---
+++ src/start/start.test.js
@@ -0,0 +1,34 @@
+const { start } = require('./start')
+const { registerHtml } = require('../dom')
+const html = registerHtml()()
+
+describe('start', () => {
+  describe('without globalSpace', () => {
+    it('should return function that can mount', () => {
+      const startNoGlobal = start()
+
+      const mockSelector = document.createElement('div')
+      const mockComponent = () => {
+        return html`<div><h1>Mock Component</h1></div>`
+      }
+      startNoGlobal(mockSelector, mockComponent)
+
+      expect(mockSelector.innerHTML).toBe('<div><h1>Mock Component</h1></div>')
+    })
+  })
+
+  describe('with globalSpace', () => {
+    it('should return function that can mount', () => {
+      const mockSpace = {}
+      const startWithGlobal = start(mockSpace)
+
+      const mockSelector = document.createElement('div')
+      const mockComponent = () => {
+        return html`<div><h1>Mock Component</h1></div>`
+      }
+      startWithGlobal(mockSelector, mockComponent)
+
+      expect(mockSelector.innerHTML).toBe('<div><h1>Mock Component</h1></div>')
+    })
+  })
+})

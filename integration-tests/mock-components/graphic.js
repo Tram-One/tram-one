08672e5b7e635d3c9be50537978005ff362c7468
JRJurman <jrjurman@gmail.com>
2019-12-22 13:23:02 -0500

    respond to PR feedback

--- integration-tests/mock-components/graphic.js
+++ integration-tests/mock-components/graphic.js
@@ -2,7 +2,7 @@ const { registerSvg } = require('../../src/tram-one')
 const svg = registerSvg()
 
 module.exports = () => svg`
-<svg viewBox="0 0 864 864">
-	<circle data-testid="graphic-circle" fill="#FDF491" cx="100" cy="100" r="20"/>
-</svg>
+	<svg viewBox="0 0 864 864">
+		<circle data-testid="graphic-circle" fill="#FDF491" cx="100" cy="100" r="20"/>
+	</svg>
 `

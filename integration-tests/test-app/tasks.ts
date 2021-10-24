7e8d7fd30fa409e5ac6ae1564b0f0c4970f3370d
JRJurman <j.r.jurman@gmail.com>
2021-10-24 14:51:29 -0400

    clean up layout for tasks test component

--- integration-tests/test-app/tasks.ts
+++ integration-tests/test-app/tasks.ts
@@ -24,12 +24,24 @@ export default () => {
 	const taskList = tasks.map((task) => html`<li>${task}</li>`);
 	return html`
 		<div>
-			<label for="new-task-label">New Task Label</label>
-			<p>${newTask.label.length} / 255</p>
-			<input id="new-task-label" name="new-task-label" value=${newTask.label} oninput=${updateNewTask} tabindex="0" />
-			<label for="new-task-type">New Task Type</label>
-			<p>${newTask.type.length} / 255</p>
-			<input id="new-task-type" name="new-task-type" value=${newTask.type} oninput=${updateNewTaskType} tabindex="0" />
+			<p>
+				<label for="new-task-label">New Task Label</label> (${newTask.label.length} / 255)
+				<br />
+				<input id="new-task-label" name="new-task-label" value=${newTask.label} oninput=${updateNewTask} tabindex="0" />
+			</p>
+
+			<p>
+				<label for="new-task-type">New Task Type</label> (${newTask.type.length} / 255)
+				<br />
+				<input
+					id="new-task-type"
+					name="new-task-type"
+					value=${newTask.type}
+					oninput=${updateNewTaskType}
+					tabindex="0"
+				/>
+			</p>
+
 			<button onclick=${addTask}>Add New Task</button>
 			<ul>
 				${taskList}

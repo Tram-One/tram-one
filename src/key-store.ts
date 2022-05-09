f40272fa5c4674aeda35a7a7659ad955a2c38e3d
JRJurman <j.r.jurman@gmail.com>
2022-05-09 00:16:20 -0400

    remove unused functions from key-store

--- src/key-store.ts
+++ src/key-store.ts
@@ -16,16 +16,6 @@ const newDefaultKeyStore = () => {
 export const { setup: setupKeyStore, get: getKeyStore, set: setKeyStore } = buildNamespace(newDefaultKeyStore);
 
 /**
- * clear the key store
- * usually called when we want to empty the key store
- */
-export const clearKeyStore = (keyStoreName: string) => {
-	const keyStore = getKeyStore(keyStoreName);
-
-	Object.keys(keyStore).forEach((key) => delete keyStore[key]);
-};
-
-/**
  * increment (or set initial value) for the keyStore
  */
 export const incrementKeyStoreValue = (keyStoreName: string, key: string) => {
@@ -40,9 +30,3 @@ export const decrementKeyStoreValue = (keyStoreName: string, key: string) => {
 	const keyStore = getKeyStore(keyStoreName);
 	keyStore[key]--;
 };
-
-/**
- * restore the key store to a previous value
- * usually used when we had to interrupt the processing of keys
- */
-export const restoreKeyStore = setKeyStore;

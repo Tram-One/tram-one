750614e7f686f1c67a1f5ce934eeabaa9e4c5f10
JRJurman <j.r.jurman@gmail.com>
2022-06-12 13:51:22 -0400

    Export raw method for observable stores

---
+++ src/raw.ts
@@ -0,0 +1,13 @@
+const { raw } = require('@nx-js/observer-util');
+/**
+ * @name raw
+ * @link https://tram-one.io/#raw
+ * @description
+ * Function that which takes in an observable object, and returns the raw value.
+ * This is intended for copying values
+ *
+ * @param observable
+ *
+ * @returns plain javascript object with no observability
+ */
+export default raw;
--- src/tram-one.ts
+++ src/tram-one.ts
@@ -3,6 +3,7 @@ export { default as useEffect } from './use-effect';
 export { default as useUrlParams } from './use-url-params';
 export { default as useStore } from './use-store';
 export { default as useGlobalStore } from './use-global-store';
+export { default as raw } from './raw';
 export { default as start } from './start';
 export {
 	ElementOrSelector,
